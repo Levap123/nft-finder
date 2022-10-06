6708778a4965d4f4c01ae9032a86f5d60ab111a7
Levap123 <96424716+Levap123@users.noreply.github.com>
2022-10-06 16:09:56 +0600

    v1

---
+++ src/handler/nft.py
@@ -0,0 +1,63 @@
+import json
+
+from flask import request, make_response, render_template
+from flask_restful import Resource
+import requests
+from src.models.nftmodel import NftModel
+
+
+class Nft(Resource):
+
+    def get(self):
+        # get argument from params
+        args = request.args
+        # nft address
+        data = args['q']
+
+        # set content type for html rendering
+        headers = {
+            "Content-Type": "text/html",
+
+        }
+
+        # search in db
+        model = NftModel.search_by_name(data)
+
+        # if model found in db, just return it to our template
+        if model:
+            payload = model.to_dict()
+            return make_response(render_template('nft_info.html', payload=payload), headers)
+
+        # else response with blockchain api
+        mainnet = requests.get(
+            "https://api.theblockchainapi.com/v1/solana/nft",
+            params={
+                # nft address searching param
+                'mint_address': data,
+                'network': 'mainnet-beta'
+            },
+            headers={
+                'APISecretKey': 'cLLuQFfSTEWwfDD',
+                'APIKeyId': 'QhWaegPvml7LnxB'
+            }
+        )
+
+        # all info about nft
+        info = mainnet.json()
+
+        # parse json
+        try:
+            payload = {
+                "name": info["off_chain_data"]["name"],
+                "address": info["mint"],
+                "image": info["off_chain_data"]["image"],
+                "description": info["off_chain_data"]["description"],
+                "uri": info["data"]["uri"],
+            }
+        except KeyError:
+            return make_response(render_template('error.html'), 404, headers)
+
+        nft_model = NftModel(**payload)
+
+        nft_model.save_to_db()
+        return make_response(render_template('nft_info.html', payload=payload), headers)
---
+++ src/models/nftmodel.py
@@ -0,0 +1,25 @@
+from src.db import db
+from sqlalchemy_serializer import SerializerMixin
+
+
+class NftModel(db.Model, SerializerMixin):
+    __tablename__ = 'nfts'
+
+    serialize_only = ('id', 'name', 'description', 'image', 'uri', 'address')
+
+    # nft entity fields
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), unique=True)
+    description = db.Column(db.String(255))
+    image = db.Column(db.String(255))
+    uri = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+
+    # safe nft to db
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def search_by_name(cls, address):
+        return cls.query.filter_by(address=address).first()
