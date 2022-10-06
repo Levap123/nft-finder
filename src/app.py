d451670ae90d858ca40158c40e1deb91a3c134c9
Levap123 <96424716+Levap123@users.noreply.github.com>
2022-10-06 18:09:07 +0600

    aaa

--- src/app.py
+++ src/app.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run(debug=False, port="8080")
+    app.run(debug=False, port="17792")
